bc5f0ee425881f823b569f577b9ef4f9201ff2b8
Karl Janson <karl.janson@taltech.com>
2021-11-14 23:00:51 +0200

    Added reading camera calibration data and publishing of compressed images

--- src/pycam_lib/publisher.py
+++ src/pycam_lib/publisher.py
@@ -1,18 +1,23 @@
 import os
 import time
 
+import cv2
+import numpy as np
 import rospy
+import yaml
 from cv_bridge import CvBridge
-from sensor_msgs.msg import CameraInfo, Image
+from sensor_msgs.msg import CameraInfo, CompressedImage, Image
+from turbojpeg import TJPF_RGBA, TurboJPEG
 
 from pycam_lib.log import PyCamLog
+# from pycam_lib.driver import DriverException
 
 TOPIC_CAM_INFO = 'camera_info'
 TOPIC_IMG_RAW = 'image_raw'
-TOPIC_IMG_COMPRESSED = 'image_compressed'
+TOPIC_IMG_COMPRESSED = 'compressed'
 
 class Publisher:
-    def __init__(self, topic_prefix='/pycam', queue_size=10, calib_file=''):
+    def __init__(self, topic_prefix='/pycam', queue_size=1, calib_file=''):
         '''
         Publishes camera frames and data as ROS topics
 
@@ -26,15 +31,17 @@ class Publisher:
 
         info_topic = os.path.join(topic_path, TOPIC_CAM_INFO)
         img_raw_topic = os.path.join(topic_path, TOPIC_IMG_RAW)
-        img_compressed_topic = os.path.join(topic_path, TOPIC_IMG_COMPRESSED)
+        img_compressed_topic = os.path.join(img_raw_topic, TOPIC_IMG_COMPRESSED)
 
-        # TODO Compressed video
         self.info_pub = rospy.Publisher(info_topic, CameraInfo, queue_size=queue_size)
         self.img_pub = rospy.Publisher(img_raw_topic, Image, queue_size=queue_size)
+        self.compressed_pub = rospy.Publisher(img_compressed_topic, CompressedImage, queue_size=queue_size)
 
         self.camera_info = self.__get_camera_info(calib_file)
 
         self.bridge = CvBridge()
+
+        self.jpeg = TurboJPEG()
 
     def __get_camera_info(self, calib_file):
         '''
@@ -49,8 +56,29 @@ class Publisher:
         '''
         
         PyCamLog.debug("Camera info extraction")
-
+            
         cam_info = CameraInfo()
+
+        if calib_file != '':
+
+            calib_file = os.path.abspath(os.path.expanduser(calib_file))
+
+            # if not os.path.isfile(calib_file):
+            #     raise DriverException("Calibration file does not exist!")
+
+            # if not os.access(calib_file, os.R_OK):
+            #     raise DriverException("Cannot read calibration file!")
+
+            with open(calib_file, 'r') as f:
+                params = yaml.load(f)
+
+                cam_info.height = params['image_height']
+                cam_info.width = params['image_width']
+                cam_info.distortion_model = params['distortion_model']
+                cam_info.K = params['camera_matrix']['data']
+                cam_info.D = params['distortion_coefficients']['data']
+                cam_info.R = params['rectification_matrix']['data']
+                cam_info.P = params['projection_matrix']['data']
 
         return cam_info
 
@@ -94,8 +122,14 @@ class Publisher:
             image: OpenCV image to publish in ROS
             stamp: timestamp to write on the image
         '''
+        compressed_image = np.array(self.jpeg.encode(image)).tostring()
 
-        pass
+        msg = CompressedImage()
+        msg.header.stamp = stamp
+        msg.format = "jpeg"
+        msg.data = compressed_image
+
+        self.compressed_pub.publish(msg)
 
     def publish(self, image):
         '''
