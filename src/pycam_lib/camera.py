Fixed GST buffer overflow bug. Did some code cleanup. Also implemented GST message reading

src/pycam_lib/camera.py
@@ -2,9 +2,8 @@ import time
 
 import cv2
 import gi
-import rospy
-
 import numpy as np
+import rospy
 
 from pycam_lib.log import PyCamLog
 from pycam_lib.publisher import Publisher
@@ -13,9 +12,9 @@ gi.require_version('Gst', '1.0')
 gi.require_version('GstBase', '1.0')
 gi.require_version('GstVideo', '1.0')
 
-from gi.repository import GLib, GObject, Gst, GstBase, GstVideo
+from gi.repository import GLib, GObject, Gst, GstApp, GstBase, GstVideo
 
-APPSINK_STR = 'appsink name=appsink sync=false'
+APPSINK_STR = 'appsink name=appsink'
 
 # How often to print average FPS
 FPS_AVG_TIME = 10
@@ -59,8 +58,9 @@ class Camera:
     def __init__(self, publisher=None, pipeline_str=None, 
                  measure_fps=False, print_fps_values=False, display_fps=False):
         
-        self.__pipeline_str = pipeline_str
         self.__publisher = publisher
+        self.__shutdown = False
+        self.__callback_error = None
 
         # For FPS calculation
         self.__measure_fps = measure_fps
@@ -79,9 +79,9 @@ class Camera:
         __pipeline = None
 
         # Set up camera
-        self.__setup()
+        self.__setup(pipeline_str)
 
-    def __setup(self):
+    def __setup(self, pipeline_str):
 
         '''
         Sets up and opens video input and reads back video parameters
@@ -89,24 +89,34 @@ class Camera:
         PyCamLog.info("Begin camera setup")
 
         # Open GStreamer pipeline
-        if not '!' in self.__pipeline_str:
+        if not '!' in pipeline_str:
             raise CameraException("Pipeline with only a single component!?!?")
 
-        if self.__pipeline_str.strip()[-1] == '!':
-            self.__pipeline_str = self.__pipeline_str.strip()[0:-1].strip()
+        if pipeline_str.strip()[-1] == '!':
+            pipeline_str = pipeline_str.strip()[0:-1].strip()
 
-        pipeline_last_element_name = self.__pipeline_str.strip().split('!')[-1].strip().split(' ')[0]
+        pipeline_last_element = pipeline_str.strip().split('!')[-1]
+        pipeline_last_element_name = pipeline_last_element.strip().split(' ')[0]
         
-        # Reject the sink in pipeline and substitute it with our own
-        if 'sink' in pipeline_last_element_name:
-            self.__pipeline_str = self.__pipeline_str[0:self.__pipeline_str.rfind('!')].strip()
+        # If the user already put an appsink in the pipeline, then use his/her appsink
+        if 'appsink' in pipeline_last_element_name:
+            if 'name=' in pipeline_last_element:
+                raise CameraException("Please remove 'name=' parameter from appsink in the pipeline")
+            else:
+                pipeline_str += ' name=appsink'
+
+        # If there is some other sink, then reject the sink in pipeline and substitute it with our own
+        elif 'sink' in pipeline_last_element_name:
+            pipeline_str = pipeline_str[0:pipeline_str.rfind('!')].strip()
             PyCamLog.warn(f"Removed '{pipeline_last_element_name}' from the pipeline "
-                           "as a properly configured appsink will be automatically added")
+                           "An appsink was automatically added")
 
-        self.__pipeline_str += f' ! {APPSINK_STR}'
+        # If no appsink was found, add it
+        if not 'appsink' in pipeline_last_element_name:
+            pipeline_str += f' ! {APPSINK_STR}'
 
-        PyCamLog.info(f"Opening pipeline: {self.__pipeline_str}")
-        self.__pipeline = Gst.parse_launch(self.__pipeline_str)
+        PyCamLog.info(f"Opening pipeline: {pipeline_str}")
+        self.__pipeline = Gst.parse_launch(pipeline_str)
 
         if self.__pipeline is None:
             raise CameraException("GStreamer pipeline parsing failed! "
@@ -114,11 +124,9 @@ class Camera:
 
         # Connect the appsink to our program
         appsink = self.__pipeline.get_by_name('appsink')
+
         appsink.set_property('emit-signals', True)
         appsink.connect('new-sample', self.__new_sample_callback)
-
-        # Create GStreamer's main loop
-        self.__gst_mainloop = GObject.MainLoop.new(None, False)
 
         PyCamLog.debug("Camera setup complete")
 
@@ -170,13 +178,12 @@ class Camera:
         
         if result:
             # Get sample's parameters
-            caps_format = sample.get_caps().get_structure(0)
+            caps = sample.get_caps()
 
-            frmt_str = caps_format.get_value('format') 
+            frmt_str = caps.get_structure(0).get_value('format')
+            h = caps.get_structure(0).get_value('height')
+            w = caps.get_structure(0).get_value('width')
 
-            w, h = caps_format.get_value('width'), caps_format.get_value('height')
-
-            # print(frmt_str)
             # Assemble an image from the buffer
             image = np.frombuffer(mapinfo.data, np.uint8)
             image = np.reshape(image, [h, w,  -1])
@@ -198,10 +205,10 @@ class Camera:
                     image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_YUY2)
 
                 else:
-                    raise CameraException("Unknown format!")
-                    self.stop()
+                    self.__callback_error = f"Unknown image format: {frmt_str}"
                     return Gst.FlowReturn.ERROR
 
+            # FPS
             if self.__display_fps:
                 if self.__last_avg_fps:
                     text = f"AVG FPS: {self.__last_avg_fps}"
@@ -214,38 +221,82 @@ class Camera:
 
             # Publish the image
             if self.__publisher is None:
-                
-                # Since we are in the callback function, 
-                # ROS printing does not work.
-                print("PYCAM: ERROR: Publisher is None!")
-                raise CameraException("Publisher is None!")
+                self.__callback_error = f"Unknown image format: {frmt_str}"
                 return Gst.FlowReturn.ERROR
 
             self.__publisher.publish(image)
+            
+            # Clear the buffer
+            buf.unmap(mapinfo)
 
         return Gst.FlowReturn.OK
 
-    def start(self):
+    def run(self):
         '''
-        Starts GStreamer's main loop
+        Starts runs the camera loop
         '''
+
+        PyCamLog.debug("Starting GStreamer main loop")
         
+        # For FPS calculation
         curr_time = time.time()
         self.__prev_frame_time = curr_time
         self.__fps_measure_time_start = curr_time
 
-        PyCamLog.debug("Starting GStreamer main loop")
+        # Start the pipeline
         self.__pipeline.set_state(Gst.State.PLAYING)
-        self.__gst_mainloop.run()
+        bus = self.__pipeline.get_bus()
 
-        PyCamLog.debug("GStreamer main loop has stopped")
+        # Monitor the communication bus for messages from GST
+        while not self.__shutdown:
+            message = bus.timed_pop_filtered(10000, Gst.MessageType.ANY)
+
+            if message:
+                if message.type == Gst.MessageType.ERROR:
+                    err, debug = message.parse_error()
+                    PyCamLog.error(f"GST ERROR from element {message.src.get_name()}: {err}")
+                    PyCamLog.debug(f"GST ERROR debug info: {debug}")
+                    break
+
+                elif message.type == Gst.MessageType.EOS:
+                    PyCamLog.info(f"GST ERROR debug info: {debug}")
+                    break
+
+                elif message.type == Gst.MessageType.STATE_CHANGED:
+                    if isinstance(message.src, Gst.Pipeline):
+                        old_state, new_state, pending_state = message.parse_state_changed()
+                        PyCamLog.debug(f"Pipeline state changed from {old_state.value_nick} to {new_state.value_nick}.")
+
+                elif message.type == Gst.MessageType.QOS:
+                    qos_msg = message.parse_qos_stats()
+                    PyCamLog.debug(f"GST QoS warning (dropped frame?)")
+
+                elif message.type == Gst.MessageType.WARNING:
+                    warn, debug = message.parse_qos_stats()
+                    PyCamLog.warn(f"GST WARN from element {message.src.get_name()}: {warn}")
+                    PyCamLog.debug(f"GST WARN debug info: {debug}")
+
+                elif message.type == Gst.MessageType.INFO:
+                    info = message.parse_info()
+                    PyCamLog.info(f"GST INFO Message: {info}")
+
+                else:
+                    PyCamLog.debug(f"Unprocessed GST message received. MSG Type: {message.type}")
+
+            if self.__callback_error:
+                self.__pipeline.set_state(Gst.State.NULL)
+                raise CameraException(self.__callback_error)
+
+        # We are exiting. Stop the pipeline
+        self.__pipeline.set_state(Gst.State.NULL)
+        PyCamLog.debug("Camera main loop has stopped")
 
     def stop(self):
         '''
-        Stops GStreamer's main loop and performs cleanup
+        Stops the main loop and performs cleanup
         '''
 
-        PyCamLog.debug("Stopping GStreamer main loop")
+        PyCamLog.debug("Stopping camera main loop")
 
-        self.__gst_mainloop.quit()
-        self.__pipeline.set_state(Gst.State.NULL)
+        self.__shutdown = True
+        