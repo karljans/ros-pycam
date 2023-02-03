3bcfdce3132f3c0db55edb61f0abdccdb7d59d44
Jetson Nano <nano@example.com>
2023-02-03 16:58:09 +0200

    Fixed error handling in publisher

--- src/pycam_lib/publisher.py
+++ src/pycam_lib/publisher.py
@@ -25,7 +25,7 @@ class PublisherException(Exception):
 
     def __init__(self, message):
         self.message = message
-        super()._init_(self.message)
+        super().__init__(self.message)
 
     def __str__(self):
         return "Publisher Exception: " + str(self.message)
